bfbcea6646d6ac2ad9e602efaeae4e80be4134ae
Mawahid <mawahid.husnain@gmail.com>
2023-11-13 08:15:32 +0500

    fixed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='math-quiz',
+    name='math_quiz',
     version='0.1',
     packages=find_packages(),
     install_requires=[
@@ -9,7 +9,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'math_quiz=math_quiz.math_quiz:main',
+            'math_quiz=math_quiz.math_quiz:math_quiz',
         ],
     },
 )
